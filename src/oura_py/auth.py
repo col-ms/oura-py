616b725b6da22c3055db61c9765ce07e4b2036ac
Collin Smith <collinmsmith22@gmail.com>
2025-02-11 16:08:45 -0500

    add params option in request handler

--- src/oura_py/auth.py
+++ src/oura_py/auth.py
@@ -5,7 +5,9 @@ class PersonalTokenRequestHandler:
     def __init__(self, personal_access_token):
         self.personal_access_token = personal_access_token
 
-    def make_request(self, url: str, method: str = "GET") -> requests.Response:
+    def make_request(
+        self, url: str, method: str = "GET", params: dict = {}
+    ) -> requests.Response:
         if not url:
             raise TypeError("URL is required")
 
@@ -15,7 +17,7 @@ class PersonalTokenRequestHandler:
         request_method = requests.post if method == "POST" else requests.get
         headers = {"Authorization": f"Bearer {self.personal_access_token}"}
 
-        response = request_method(url, headers=headers)
+        response = request_method(url, headers=headers, params=params)
         response.raise_for_status()
 
         return response
